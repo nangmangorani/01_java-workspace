75022d1152097e90a08f96b91e92d144135a405a
nangmangorani <dltmd310@gmail.com>
2023-07-10 21:17:28 +0900

    0710

---
+++ SoloStudy/src/static_study/Run.java
@@ -0,0 +1,15 @@
+package static_study;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		Static_Study a = new Static_Study();
+		
+		System.out.println(Static_Study.name);
+		
+		
+		
+	}
+
+}
---
+++ SoloStudy/src/static_study/Static_Study.java
@@ -0,0 +1,24 @@
+package static_study;
+
+public class Static_Study {
+	
+	public static final String name = "MangKyu";
+    
+	
+	public static int add(int x, int y) {
+		return x + y;
+	}
+	
+	
+	public int min(int x, int y) {
+		return x - y;
+	}
+	
+	
+	
+	
+	
+
+}
+
+
